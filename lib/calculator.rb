3d8df1bbf64caac9ea55ee68d1861450344c63ef
adrianke77 <adrianke77@gmail.com>
2017-02-01 13:41:08 +0800

    operation test

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -8,21 +8,26 @@ class Calculator
 
   def reset(result)
     @result = result
+    self
   end
 
   def add(value_to_add)
     @result +=value_to_add
+    self
   end
 
   def subtract(value_to_subtract)
     @result -=value_to_subtract
+    self
   end
 
   def mult(value_to_mult)
     @result *=value_to_mult
+    self
   end
 
   def divide(value_to_divide)
     @result /=value_to_divide
+    self
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,51 +13,60 @@ describe Calculator do
   end
   describe 'Result' do
     it 'should return the current results' do
-    expect(@my_calculator.result).to eq(1)
+      expect(@my_calculator.result).to eq(1)
     end
   end
   describe 'Reset' do
     it 'should reset the calculator to given value' do
-    @my_calculator.reset(5)
-    expect(@my_calculator.result).to eq(5)
+      @my_calculator.reset(5)
+      expect(@my_calculator.result).to eq(5)
     end
   end
   describe 'Add' do
     it 'should add to the calculator' do
-    @my_calculator.add(5)
-    expect(@my_calculator.result).to eq(10)
+      @my_calculator.add(5)
+      expect(@my_calculator.result).to eq(10)
     end
   end
   describe 'Subtract' do
     it 'should subtract from the calculator' do
-    @my_calculator.subtract(3)
-    expect(@my_calculator.result).to eq(7)
+      @my_calculator.subtract(3)
+      expect(@my_calculator.result).to eq(7)
     end
   end
   describe 'Multiply' do
     it 'should multiply to the calculator' do
-    @my_calculator.mult(3)
-    expect(@my_calculator.result).to eq(21)
+      @my_calculator.mult(3)
+      expect(@my_calculator.result).to eq(21)
     end
   end
-   describe 'divide' do
+  describe 'divide' do
     it 'should divide calculator result by value' do
-    @my_calculator.divide(7)
-    expect(@my_calculator.result).to eq(3)
+      @my_calculator.divide(7)
+      expect(@my_calculator.result).to eq(3)
     end
   end
   describe 'chain' do
-   it 'should allow for chaining' do
-   @my_calculator.reset(5)
-   @my_calculator.add(6).subtract(1)
-   expect(@my_calculator.result).to eq(10)
-   @my_calculator.reset(5)
-   @my_calculator.divide(5).mult(10)
-   expect(@my_calculator.result).to eq(10)
-   @my_calculator.reset(10)
-   @my_calculator.subtract(5).add(2)
-   expect(@my_calculator.result).to eq(7)
-   end
- end
-
+    it 'should allow for chaining' do
+      @my_calculator.reset(5)
+      @my_calculator.add(6).subtract(1)
+      expect(@my_calculator.result).to eq(10)
+      @my_calculator.reset(5)
+      @my_calculator.divide(5).mult(10)
+      expect(@my_calculator.result).to eq(10)
+      @my_calculator.reset(10)
+      @my_calculator.subtract(5).add(2)
+      expect(@my_calculator.result).to eq(7)
+    end
+  end
+  describe 'operation' do
+    it 'should apply the specified operation' do
+      @my_calculator.reset(1)
+      @my_calculator.operation('add',5)
+      @my_calculator.operation('subtract',4)
+      @my_calculator.operation('multiply',3)
+      @my_calculator.operation('divide',2)
+      expect(@my_calculator.result).to eq(3)
+    end
+  end
 end
