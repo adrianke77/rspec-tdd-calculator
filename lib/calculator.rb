aafd891878a86ff200e01a846a375979480d48fe
glensin <glensin@Sin-Chin-Guan-Glens-MacBook-Air.local>
2017-02-01 13:10:32 +0800

    changes

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,15 +2,6 @@ class Calculator
 
   def initialize(result)
     @result = result
-<<<<<<< HEAD
   end
-
-end
-=======
-
-
-  end
-
   attr_reader :result
-end 
->>>>>>> 71ea54bc4b367307521f221058ec9580cdcfbb36
+end
