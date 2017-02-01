2e22d52f21ddc19aff6b42a8418b6829d9032658
adrianke77 <adrianke77@gmail.com>
2017-02-01 12:47:33 +0800

    added initialization test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,12 +3,12 @@ require_relative '../lib/calculator.rb'
 
 describe Calculator do
   before(:context) do
-    @calculator = Calculator.new(1)
+    @my_calculator = Calculator.new(1)
     # initialize calculator
   end
     describe 'Initialization' do
     it 'is an instance of the Calculator class' do
-      expect(@mailbox1).to be_instance_of(Calculator)
+      expect(@my_calculator).to be_instance_of(Calculator)
     end
   end
   # Tests go here
